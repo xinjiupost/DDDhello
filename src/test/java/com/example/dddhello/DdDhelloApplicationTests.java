b0a8a89f8eb8ea44205aa7eb88d1cb4845b4b535
zhangsan <xinjiupost@outlook.com>
2024-04-01 22:11:55 +0800

    我的第一次commit提交，commit整个文件夹

---
+++ src/test/java/com/example/dddhello/DdDhelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dddhello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdDhelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
